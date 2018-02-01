c1d97ca34531e0993320910d7a2f7ad1d186feb2
braeden-smith <34383327+braeden-smith@users.noreply.github.com>
2018-02-01 13:25:20 -0700

    Add files via upload

---
+++ PythonVariable.py
@@ -0,0 +1,9 @@
+#Braeden Smith Python Variables Challenge
+
+#This asks for the name of what you want
+name = input("Enter a name of something: ")
+
+#This prints out the banner requested
+print "------------"
+print ".::"+ name +"::."
+print "------------"
